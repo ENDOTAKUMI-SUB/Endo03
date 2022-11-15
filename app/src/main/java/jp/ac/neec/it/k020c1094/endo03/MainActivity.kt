1745ea8c360c3c78d1f7d827939ad693a1a274de
ENDOTAKUMI <en0605taku@gmail.com>
2022-11-15 12:30:19 +0900

    first commit

---
+++ app/src/main/java/jp/ac/neec/it/k020c1094/endo03/MainActivity.kt
@@ -0,0 +1,16 @@
+package jp.ac.neec.it.k020c1094.endo03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
+
